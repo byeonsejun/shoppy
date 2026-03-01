6e7757d479e4cf4312de12b708f720e8183c27c5
byeonsejun <qustpwns93@gmail.com>
2026-03-01 18:24:51 +0900

    main component qick loading

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
 import ProtectedRoute from './pages/ProtectedRoute';
 
-// Code Splitting: Home을 제외한 나머지 컴포넌트는 지연 로딩 적용
-const Home = React.lazy(() => import('./pages/Home'));
+// Code Splitting: Home은 즉시 로딩하고, 나머지는 지연 로딩 적용
 const AllProducts = React.lazy(() => import('./pages/AllProducts'));
 const ProductDetail = React.lazy(() => import('./pages/ProductDetail'));
 const NewProduct = React.lazy(() => import('./pages/NewProduct'));
